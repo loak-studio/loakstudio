f6e8996578c1fcbb5168af9dbc8666b121ebed9e
Benjamin Geets <benjamingeets@tutamail.com>
2022-03-14 20:31:58 +0100

    feat: seo & footer links before

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -36,8 +36,8 @@ export default () => {
             <ul class="flex flex-row items-center gap-x-4">
                 {links.map(link => {
                     return (
-                        <li className='p-1 transition rounded-sm hover:bg-black hover:bg-opacity-10'>
-                            <a target="_blank" rel="noopener noreferrer" href={link.url} className="transition hover:text-primary-darkest">
+                        <li className='relative p-1 transition rounded-sm hover:bg-black hover:bg-opacity-10'>
+                            <a target="_blank" rel="noopener noreferrer" href={link.url} className="transition before:absolute before:inset-0 hover:text-primary-darkest">
                                 <Icons name={link.icon} />
                                 <span className='sr-only'>{link.name}</span>
                             </a>
