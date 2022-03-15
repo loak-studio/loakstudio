a62cd1fe2fe35f294ec5a47ed8848730ceabf6a9
lava <lava@lava>
2022-03-15 20:42:15 +0100

    Refactor: Navbar.jsx and index.astro

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,14 +3,11 @@ import Button from './Button.jsx'
 
 export default () => {
     return (
-        <nav class="flex justify-between pt-8 mb-8 px-4 md:px-8">
+        <nav class="flex justify-start pt-8 mb-8 px-4 md:px-8">
             <a class="flex items-center text-3xl text-primary" href="/">
                 <Icons name="loak"/>
                 <h1 className='ml-4'>loak.studio</h1>
             </a>
-                <Button href='#' classes="hidden sm:flex">
-                    <Icons name="send"/> Nous contacter
-                </Button>
         </nav>
     )
 }
