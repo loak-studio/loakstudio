69365e8e7cc72e7e9d291ab8a4262b5d7bede342
Benjamin Geets <benjamin@geets.dev>
2022-03-28 19:49:30 +0200

    feat(tailwind): colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,23 @@ module.exports = {
     "./src/**/*.{astro,html,js,jsx,svelte,css}"
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        black:'#08221D',
+        white:'#FBFFFD',
+        primary:{
+          'DEFAULT':'#00A99D',
+        },
+        secondary:{
+          'DEFAULT':'#DD7D5F',
+          'darkest':'#003935'
+        },
+        grey:{
+          'light':'#F5F5F5',
+          'DEFAULT':'#A0A0A0'
+        }
+      }
+    },
   },
   plugins: [],
 }
