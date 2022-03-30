25d25b6c556b5408584bbb6befcf5530fad588fb
benjamingeets <benjamingeets@tutamail.com>
2022-03-30 20:35:18 +0200

    feat: loop

--- src/components/componentRender.jsx
+++ src/components/componentRender.jsx
@@ -2,8 +2,12 @@ const ComponentRender = ({component})=>{
     switch(component.__component){
         case "page.hero":
             return <p>Hero</p>
+        case "page.discover":
+            return <p>discover</p>
+            case "page.ctacontact":
+                return <p>ctacontact</p>
         default:
-            return <p>unknow</p>
+            return <p>{component.__component}</p>
     }
 }
 
