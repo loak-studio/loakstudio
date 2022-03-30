57f04e6df5899f11aa5995c72155264d291595fb
benjamingeets <benjamingeets@tutamail.com>
2022-03-30 16:57:50 +0200

    feat(homepage): query from API

--- src/functions.js
+++ src/functions.js
@@ -1,6 +1,6 @@
 
 import {API_URL} from "./config"
-export const getData = async (path) =>{
-    const req = await fetch(`${API_URL}/api/${path}?populate=*`)
+export const getData = async (path,params) =>{
+    const req = await fetch(`${API_URL}/api/${path}?populate=*${params?params:''}`)
     return await req.json()
 }
