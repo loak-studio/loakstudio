8ab807177ecb940352dd2be239044d6cb90a1e1b
benjamingeets <benjamingeets@tutamail.com>
2022-04-03 15:28:59 +0200

    rebuild

--- src/functions.js
+++
@@ -1,5 +0,0 @@
-import {API_URL} from "./config"
-export const getData = async (path,params) =>{
-    const req = await fetch(`${API_URL}/api/${path}?populate=*${params?params:''}`)
-    return await req.json()
-}
--- src/scripts/app.js
+++
@@ -1,4 +0,0 @@
-import unpoly from 'unpoly'
-
-up.link.config.followSelectors.push('a[href]')
-up.form.config.submitSelectors.push(['form'])
