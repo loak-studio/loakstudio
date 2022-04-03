bf1a6bbda771d56b203f0df2059a53ce3280a0dd
benjamingeets <benjamingeets@tutamail.com>
2022-04-03 18:51:48 +0200

    feat(nav): hamburger menu

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,3 +1,20 @@
 import unpoly from 'unpoly'
 up.link.config.followSelectors.push('a[href]')
 up.form.config.submitSelectors.push(['form'])
+
+
+
+up.compiler('body',()=>{
+    const button = document.querySelector('#toggle-menu')
+
+    button.addEventListener('click', function () {
+        const menu = document.querySelector('#menu')
+        menu.classList.toggle('-translate-x-full')
+        if(menu.classList.contains('-translate-x-full')) {
+            button.textContent = "Menu"
+        }
+        else{
+            button.textContent = "Fermer"
+        }
+    })
+})
