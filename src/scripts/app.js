bd03b0e31f9084628e0bf7a95fbada9e1b7380fc
benjamingeets <benjamingeets@tutamail.com>
2022-04-03 15:59:33 +0200

    build: blobity

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,4 +1,6 @@
 import unpoly from 'unpoly'
-
+import Blobity from 'blobity';
 up.link.config.followSelectors.push('a[href]')
 up.form.config.submitSelectors.push(['form'])
+const options = { color: 'rgb(180, 180, 180)' };
+new Blobity(options);
