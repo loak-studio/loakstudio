8bea0a11dcc8a3fd7ee0dfdc15778b4c864b3e07
benjamingeets <benjamingeets@tutamail.com>
2022-04-03 16:05:50 +0200

    feat:blobity

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,4 +1,14 @@
 import unpoly from 'unpoly'
+import Blobity from 'blobity';
+
 
 up.link.config.followSelectors.push('a[href]')
 up.form.config.submitSelectors.push(['form'])
+up.compiler('body',()=>{
+    const options = { 
+        color: 'rgb(180, 180, 180)' ,
+        opacity:0.5,
+        zIndex:5,
+    };
+    new Blobity(options);
+})
