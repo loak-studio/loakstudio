cd217262c65b744ac84689baa601de50b2ef5452
benjamingeets <benjamingeets@tutamail.com>
2022-04-03 16:22:51 +0200

    remove blobity

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,14 +1,5 @@
 import unpoly from 'unpoly'
-import Blobity from 'blobity';
 
 
 up.link.config.followSelectors.push('a[href]')
 up.form.config.submitSelectors.push(['form'])
-up.compiler('body',()=>{
-    const options = { 
-        color: 'rgb(180, 180, 180)' ,
-        opacity:0.5,
-        zIndex:5,
-    };
-    new Blobity(options);
-})
