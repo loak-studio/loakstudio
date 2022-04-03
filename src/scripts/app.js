240be82dde6bb48b9cf982ebd77620dc1d241177
benjamingeets <benjamingeets@tutamail.com>
2022-04-03 18:14:39 +0200

    feat: nav & footer

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,6 +1,3 @@
 import unpoly from 'unpoly'
-import Blobity from 'blobity';
 up.link.config.followSelectors.push('a[href]')
 up.form.config.submitSelectors.push(['form'])
-const options = { color: 'rgb(180, 180, 180)' };
-new Blobity(options);
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,37 @@
 module.exports = {
-  content: ["./src/**/*.astro"],
+  content: [
+    "./src/**/*.{astro,html,js,jsx,svelte,css}"
+  ],
   theme: {
-    extend: {},
+    extend: {
+      maxWidth: {
+        '8xl': '93rem'
+      },
+      fontFamily: {
+        'title': 'Lack',
+        'content': 'Montserrat'
+      },
+      colors: {
+        primary: {
+          'DEFAULT': '#00A99D',
+          'darkest': '#003935'
+        },
+        secondary: {
+          'DEFAULT': '#DD7D5F',
+        },
+        lgrey: {
+          'light': '#F5F5F5',
+          'DEFAULT': '#A0A0A0',
+          'dark': '#6B6B6B'
+        },
+        lblack: {
+          'DEFAULT': '#08221D'
+        },
+        lwhite: {
+          'DEFAULT': '#FBFFFD'
+        },
+      }
+    },
   },
   plugins: [],
 }
