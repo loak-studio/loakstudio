e640b4b723e8803ffd47310094fd8b0184afb645
benjamingeets <benjamingeets@tutamail.com>
2022-04-11 13:22:01 +0200

    feat: preload links

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,7 +1,8 @@
 import unpoly from 'unpoly'
 up.link.config.followSelectors.push('a[href]')
 up.form.config.submitSelectors.push(['form'])
-
+up.link.config.instantSelectors.push('a[href]')
+up.link.config.preloadSelectors.push('a[href]')
 
 
 up.compiler('body',()=>{
