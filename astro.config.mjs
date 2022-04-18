bec51d5f3774280347434498f8cd6c2430aee91b
benjamingeets <geets@tuta.io>
2022-04-18 22:24:32 +0200

    feat: sitema

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,12 @@
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-    site:"https://loak.studio"
+    site:"https://loak.studio",
+    integrations: [
+        sitemap({
+            filter: (page) => page !== 'https://loak.studio/send/'
+          }),
+    ],
 });
