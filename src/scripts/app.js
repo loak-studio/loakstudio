029359f35f4a8bb60f72b8a295b72698d1660903
benjamingeets <geets@tuta.io>
2022-04-20 21:25:42 +0200

    feat(home/contact): animations

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -8,66 +8,72 @@ up.form.config.submitSelectors.push(['form'])
 up.link.config.instantSelectors.push('a[href]')
 up.link.config.preloadSelectors.push('a[href]')
 
-
+let isAnimationPlayed = {
+  homepage: false,
+  contact:false
+} 
 
 up.compiler('#hero',()=>{
       animate(
         "#hero div",
         { y: [50,0], opacity:[0,1] },
-        { easing: spring(), delay:1.2 }
+        { easing: spring(), delay:(isAnimationPlayed.homepage? 0:1.2) }
       )
 
 
       // 
       
-      animate(
-        "#hero #lock",
-        {
-          opacity: [0,1],
-          x:[-50,0],
-          y:[50,0]
-        },
-        { easing: spring(), delay:0 }
-      )
 
 
-      animate(
-        "#hero #computer",
-        {
-          opacity: [0,1],
-          x:[-50,0],
-          y:[-50,0]
-        },
-        { easing: spring(), delay:0.2 }
-      )
-      animate(
-        "#hero #mail",
-        {
-          opacity: [0,1],
-          x:[50,0],
-          y:[-50,0]
-        },
-        { easing: spring(), delay:0.4 }
-      )
-      animate(
-        "#hero #figma",
-        {
-          opacity: [0,1],
-          x:[50,0],
-          y:[0,0]
-        },
-        { easing: spring(), delay:0.6 }
-      )
-
-      animate(
-        "#hero #bag",
-        {
-          opacity: [0,1],
-          x:[50,0],
-          y:[50,0]
-        },
-        { easing: spring(), delay:0.8 }
-      )
+      if(!isAnimationPlayed.homepage){
+        animate(
+          "#hero #lock",
+          {
+            opacity: [0,1],
+            x:[-50,0],
+            y:[50,0]
+          },
+          { easing: spring(), delay:0 }
+        )
+        animate(
+          "#hero #computer",
+          {
+            opacity: [0,1],
+            x:[-50,0],
+            y:[-50,0]
+          },
+          { easing: spring(), delay:0.2 }
+        )
+        animate(
+          "#hero #mail",
+          {
+            opacity: [0,1],
+            x:[50,0],
+            y:[-50,0]
+          },
+          { easing: spring(), delay:0.4 }
+        )
+        animate(
+          "#hero #figma",
+          {
+            opacity: [0,1],
+            x:[50,0],
+            y:[0,0]
+          },
+          { easing: spring(), delay:0.6 }
+        )
+  
+        animate(
+          "#hero #bag",
+          {
+            opacity: [0,1],
+            x:[50,0],
+            y:[50,0]
+          },
+          { easing: spring(), delay:0.8 }
+        )
+        isAnimationPlayed.homepage = true;
+      }
     
 })
 
@@ -99,4 +105,30 @@ up.compiler('body',()=>{
             window.scrollTo(0,0)
         }
     })
+})
+
+up.compiler("#contactform",()=>{
+  if(!isAnimationPlayed.contact){
+    animate(
+      ".message",
+      { 
+        y: [-10,0],
+      opacity:[0,1]
+     },
+      {
+        delay: stagger(0.5),
+        duration: 0.5,
+        easing: [.22, .03, .26, 1]
+      }
+    )
+    animate(
+      "#bubble",
+      {
+        x:[0,25,0],
+        rotate:[-10,0,-10]
+      },
+      { duration:5, repeat:Infinity, easing:"ease-in-out" }
+    )
+    isAnimationPlayed.contact = true
+  }
 })
