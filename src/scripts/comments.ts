5ffa38027c6d39aad42c2ba72bec6380a66ce653
benjamingeets <benjamingeets@tutamail.com>
2022-07-05 14:44:06 +0200

    feat: landing bottom

---
+++ src/scripts/comments.ts
@@ -0,0 +1,77 @@
+const container = document.querySelector("[data-references]");
+const comment = {
+  container: container.querySelector("[data-comment]"),
+  name: container.querySelector("[data-current-comment-name]"),
+  job: container.querySelector("[data-current-comment-job]"),
+  picture: container.querySelector("[data-current-comment-picture]"),
+  text: container.querySelector("[data-current-comment-text]"),
+};
+
+window.addEventListener("load", () => {
+  if (container) {
+    const buttons = container.querySelectorAll<HTMLElement>(
+      "[data-customer-name]",
+    );
+
+    let loop;
+
+    const setLoop = (index = 0) => {
+      loop = setInterval(() => {
+        index++;
+        if (index == buttons.length) {
+          index = 0;
+        }
+        updateComment(buttons, buttons[index]);
+      }, 10000);
+    };
+    setLoop();
+    buttons.forEach((button, index) => {
+      button.addEventListener("click", () => {
+        updateComment(buttons, button);
+        clearInterval(loop);
+      });
+    });
+  }
+});
+
+export {};
+
+const updateComment = (buttons, button) => {
+  const items = document.querySelectorAll("[data-comment] > *");
+  updateCommentIndicator({ buttons, button });
+  containerContentTransition(items);
+  setTimeout(() => {
+    setCommentContent(button.dataset);
+  }, 200);
+};
+
+const updateCommentIndicator = ({ buttons, button }) => {
+  buttons.forEach((item) => {
+    item.classList.remove("bg-primary-500");
+    item.classList.add("bg-gray-300");
+  });
+  button.classList.remove("bg-gray-300");
+  button.classList.add("bg-primary-500");
+};
+
+const setCommentContent = (dataset) => {
+  comment.name.textContent = dataset.customerName;
+  comment.job.textContent = dataset.customerJob;
+  comment.picture.textContent = dataset.customePicture;
+  comment.text.textContent = dataset.customerComment;
+};
+
+const containerContentTransition = (items) => {
+  items.forEach((i) => {
+    i.classList.add("opacity-0");
+    i.classList.add("-ml-16");
+    setTimeout(() => {
+      i.classList.remove("-ml-16");
+      i.classList.add("ml-16");
+    }, 200);
+    setTimeout(() => {
+      i.classList.remove("opacity-0");
+      i.classList.remove("ml-16");
+    }, 500);
+  });
+};
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,30 +1,28 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,js}'],
-	theme: {
-		extend: {
-			fontFamily: {
-				'title': 'Lack',
-				'content': 'Montserrat'
-			  },
-			colors:{
-				'primary': {
-					DEFAULT: '#388A84',
-					'50': '#A1D9D5',
-					'100': '#92D3CE',
-					'200': '#75C7C1',
-					'300': '#58BBB4',
-					'400': '#44A7A0',
-					'500': '#388A84',
-					'600': '#28625E',
-					'700': '#183A38',
-					'800': '#071211',
-					'900': '#000000'
-				  },
-			}
-		},
-	},
-	plugins: [
-		require('@tailwindcss/line-clamp')
-	],
-}
+  content: ["./src/**/*.{astro,js,ts}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        title: "Lack",
+        content: "Montserrat",
+      },
+      colors: {
+        primary: {
+          DEFAULT: "#388A84",
+          50: "#A1D9D5",
+          100: "#92D3CE",
+          200: "#75C7C1",
+          300: "#58BBB4",
+          400: "#44A7A0",
+          500: "#388A84",
+          600: "#28625E",
+          700: "#183A38",
+          800: "#071211",
+          900: "#000000",
+        },
+      },
+    },
+  },
+  plugins: [require("@tailwindcss/line-clamp")],
+};
