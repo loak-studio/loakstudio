9c2ae76963c4b81c2f7ff945ae7da56698af5758
benjamingeets <benjamingeets@tutamail.com>
2022-07-29 14:38:46 +0200

    fix(services): make animation faster

--- src/scripts/animations/services.ts
+++ src/scripts/animations/services.ts
@@ -6,8 +6,8 @@ window.addEventListener("load", function () {
       listItem,
       { x: [-30, 0], opacity: [0, 1] },
       {
-        delay: stagger(0.5),
-        duration: 1,
+        delay: stagger(0.2),
+        duration: 0.8,
         easing: spring(),
       },
     );
